ea9b53706d4d0ee6519262bc184211e3375db668
Margarita <m.novolokova@gmail.com>
2023-04-05 10:11:53 +0300

    Add query-string

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import qs from 'query-string'
 
 const httpClient = axios.create({
   // подключение к ресурсу для работы, запрос клиента за адресом сервера
@@ -6,7 +7,16 @@ const httpClient = axios.create({
 });
 
 export const getUsers = (
-  { limit, offset } //axios-запрос всегда возвращает promice => передается в userSlice(reduser) => Middleware(работает с посторонними эфектами) и dispatch(action) передается в userSlice - extraReducers и изменяется state (зв'язали  back i front)
-) => httpClient.get('/users?limit=2&offset=0');
+  { options={}} //axios-запрос всегда возвращает promice => передается в userSlice(reduser) => Middleware(работает с посторонними эфектами) и dispatch(action) передается в userSlice - extraReducers и изменяется state (зв'язали  back i front)
+) => {
+  const defaultOptions = {
+    limit:5,
+    offset:0
+  }
+  const readyOptions={
+    ...defaultOptions,
+    ...options
+  }
+  return httpClient.get(`/users?${qs.stringify(readyOptions)}`)};
 
 export const postUser = (values) => httpClient.post('/users', values);
